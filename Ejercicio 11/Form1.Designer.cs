9315c24513bcfb1199c206178f0c4db571e9a3b9
Edgar <20240109@ricaldone.edu.sv>
2025-06-13 21:53:11 -0600

    Ejercicio 11

---
+++ Ejercicio 11/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Ejercicio_11
+{
+    partial class frmCalcularPrecioDescuento
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalcularPrecioDescuento));
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.grbDescuento = new System.Windows.Forms.GroupBox();
+            this.txtDescuento = new System.Windows.Forms.TextBox();
+            this.lblEs = new System.Windows.Forms.Label();
+            this.lblPrecioProducto = new System.Windows.Forms.Label();
+            this.txtPrecioProducto = new System.Windows.Forms.TextBox();
+            this.grbDescuento.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Image = ((System.Drawing.Image)(resources.GetObject("btnBorrar.Image")));
+            this.btnBorrar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnBorrar.Location = new System.Drawing.Point(636, 163);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(109, 76);
+            this.btnBorrar.TabIndex = 21;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click_1);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.Image")));
+            this.btnSalir.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSalir.Location = new System.Drawing.Point(636, 294);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(109, 76);
+            this.btnSalir.TabIndex = 20;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Image = ((System.Drawing.Image)(resources.GetObject("btnConvertir.Image")));
+            this.btnConvertir.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnConvertir.Location = new System.Drawing.Point(636, 46);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(109, 76);
+            this.btnConvertir.TabIndex = 19;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // grbDescuento
+            // 
+            this.grbDescuento.Controls.Add(this.txtDescuento);
+            this.grbDescuento.Controls.Add(this.lblEs);
+            this.grbDescuento.Location = new System.Drawing.Point(64, 294);
+            this.grbDescuento.Name = "grbDescuento";
+            this.grbDescuento.Size = new System.Drawing.Size(512, 100);
+            this.grbDescuento.TabIndex = 22;
+            this.grbDescuento.TabStop = false;
+            this.grbDescuento.Text = "Resultado";
+            // 
+            // txtDescuento
+            // 
+            this.txtDescuento.Location = new System.Drawing.Point(26, 56);
+            this.txtDescuento.Name = "txtDescuento";
+            this.txtDescuento.ReadOnly = true;
+            this.txtDescuento.Size = new System.Drawing.Size(100, 20);
+            this.txtDescuento.TabIndex = 9;
+            // 
+            // lblEs
+            // 
+            this.lblEs.AutoSize = true;
+            this.lblEs.Location = new System.Drawing.Point(23, 32);
+            this.lblEs.Name = "lblEs";
+            this.lblEs.Size = new System.Drawing.Size(235, 13);
+            this.lblEs.TabIndex = 1;
+            this.lblEs.Text = "El precio del articulo despues del descuento es :";
+            // 
+            // lblPrecioProducto
+            // 
+            this.lblPrecioProducto.AutoSize = true;
+            this.lblPrecioProducto.Location = new System.Drawing.Point(87, 130);
+            this.lblPrecioProducto.Name = "lblPrecioProducto";
+            this.lblPrecioProducto.Size = new System.Drawing.Size(144, 13);
+            this.lblPrecioProducto.TabIndex = 23;
+            this.lblPrecioProducto.Text = "Ingrese el Precio del Articulo:";
+            // 
+            // txtPrecioProducto
+            // 
+            this.txtPrecioProducto.Location = new System.Drawing.Point(90, 163);
+            this.txtPrecioProducto.Name = "txtPrecioProducto";
+            this.txtPrecioProducto.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecioProducto.TabIndex = 24;
+            // 
+            // frmCalcularPrecioDescuento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPrecioProducto);
+            this.Controls.Add(this.lblPrecioProducto);
+            this.Controls.Add(this.grbDescuento);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnConvertir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmCalcularPrecioDescuento";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Precio Descuento";
+            this.grbDescuento.ResumeLayout(false);
+            this.grbDescuento.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.GroupBox grbDescuento;
+        private System.Windows.Forms.TextBox txtDescuento;
+        private System.Windows.Forms.Label lblEs;
+        private System.Windows.Forms.Label lblPrecioProducto;
+        private System.Windows.Forms.TextBox txtPrecioProducto;
+    }
+}
+
---
+++ Ejercicio 11/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_11
+{
+    public partial class frmCalcularPrecioDescuento : Form
+    {
+        double precioOriginal, precioFinal;
+        double porcentajeDescuento = 0.65;
+        public frmCalcularPrecioDescuento()
+        {
+            InitializeComponent();
+        }
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+
+
+            if (double.TryParse(txtPrecioProducto.Text, out precioOriginal))
+            {
+                precioFinal = precioOriginal * (1 - porcentajeDescuento); 
+                txtDescuento.Text = precioFinal.ToString("N2");
+            }
+            else
+            {
+                txtDescuento.Text = "Entrada inválida";
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnBorrar_Click_1(object sender, EventArgs e)
+        {
+            txtDescuento.Clear();
+            txtPrecioProducto.Clear();
+        }
+    }
+    
+}
